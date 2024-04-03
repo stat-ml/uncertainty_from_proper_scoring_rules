b01dac73f1a24222d80c1e42cb0b658d1f0ed369
Nikita Kotelevskii <niket0996@gmail.com>
2024-04-03 16:52:44 +0300

    Added one more script for launching experiments

--- external_repos/pytorch_cifar10/launch_resnet_ensemble.sh
+++ external_repos/pytorch_cifar10/launch_resnet_ensemble.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Define an array of loss function types
-loss_functions=("cross_entropy" "brier_score")
+#declare -a loss_functions=("cross_entropy")
 
 # Outer loop over loss function types
-for loss_function in "${loss_functions[@]}"
-do
-    # Inner loop from 5 to 50 for model_id
-    for model_id in $(seq 5 50)
+#for loss_function in "${loss_functions[@]}"
+#do
+ loss_function="cross_entropy"
+ # Inner loop from 6 to 50 for model_id
+    for model_id in $(seq 6 50)
     do
         # Run the Python command with the current model_id and loss_function
-        python main.py --model_id $model_id --architecture resnet18 --loss $loss_function
+        CUDA_VISIBLE_DEVICES=0 python main.py --model_id $model_id --architecture resnet18 --loss $loss_function
     done
-done
+#done
---
+++ external_repos/pytorch_cifar10/launch_resnet_ensemble_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define an array of loss function types
+# declare -a loss_functions=("brier_score")
+
+# Outer loop over loss function types
+# for loss_function in "${loss_functions[@]}"
+# do
+    # Inner loop from 0 to 50 for model_id
+loss_function="brier_score"
+    for model_id in $(seq 0 50)
+    do
+        # Run the Python command with the current model_id and loss_function
+        CUDA_VISIBLE_DEVICES=1 python main.py --model_id $model_id --architecture resnet18 --loss $loss_function
+    done
+# done
