7c95c6f18120dd81506e6c21fcce9dca81b6fba9
Nikita Kotelevskii <nikita.kotelevskii@skoltech.ru>
2024-04-03 15:46:18 +0400

    Bash running file corrected

--- external_repos/pytorch_cifar10/launch_resnet_ensemble.sh
+++ external_repos/pytorch_cifar10/launch_resnet_ensemble.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Loop from 1 to 50
-for model_id in $(seq 1 50)
+# Loop from 4 to 50
+for model_id in $(seq 4 50)
 do
     # Run the Python command with the current model_id
-    python main.py --model_id $model_id --architecture resnet18
+    python main.py --model_id $model_id --architecture resnet18 --loss cross_entropy
 done
 
