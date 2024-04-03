166bc86a1d0d0c6c4999ce3abc0f9e6e369c9cd1
Nikita Kotelevskii <nikita.kotelevskii@skoltech.ru>
2024-04-03 17:03:04 +0400

    Fixed bug with brier score

--- src/losses.py
+++ src/losses.py
@@ -3,25 +3,29 @@ import torch.nn as nn
 
 import torch.nn.functional as F
 
+class SphericalScoreLoss(nn.Module):
+    def __init__(self):
+        super(SphericalScoreLoss, self).__init__()
+ 
 
 class BrierScoreLoss(nn.Module):
     def __init__(self):
         super(BrierScoreLoss, self).__init__()
     
-    def forward(self, predictions, targets):
+    def forward(self, logits, targets):
         """
         Calculate the Brier Score Loss for multi-class classification
         
         Parameters:
-        - predictions: Tensor of predicted probabilities for each class (as returned by a softmax function)
+        - logits: Tensor of predicted logits for each class (as returned before a softmax function)
         - targets: Tensor of actual labels, not one-hot encoded
         
         Returns:
         - Brier score loss: Tensor
         """
         # Ensure targets are one-hot encoded
-        targets_one_hot = F.one_hot(targets, num_classes=predictions.size(1)).float()
-        
+        targets_one_hot = F.one_hot(targets, num_classes=logits.size(1)).float() 
+        predictions = F.softmax(logits, dim=-1)
         loss = torch.mean((predictions - targets_one_hot) ** 2)
         return loss
 
@@ -32,6 +36,8 @@ def get_loss_function(loss_name: str) -> torch.nn.Module:
             loss = nn.CrossEntropyLoss()
         case 'brier_score':
             loss = BrierScoreLoss() 
+        case 'spherical_score':
+            loss = SphericalScoreLoss()
         case _:
             print("No such loss")
             raise ValueError
