807c16693534dbc107644eb94868a4aaa5107169
Nikita Kotelevskii <nikita.kotelevskii@skoltech.ru>
2024-04-03 16:37:02 +0400

    Script for launching ensembles corrected

--- external_repos/pytorch_cifar10/launch_resnet_ensemble.sh
+++ external_repos/pytorch_cifar10/launch_resnet_ensemble.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Loop from 4 to 50
-for model_id in $(seq 4 50)
-do
-    # Run the Python command with the current model_id
-    python main.py --model_id $model_id --architecture resnet18 --loss cross_entropy
-done
+# Define an array of loss function types
+loss_functions=("cross_entropy" "brier_score")
 
+# Outer loop over loss function types
+for loss_function in "${loss_functions[@]}"
+do
+    # Inner loop from 5 to 50 for model_id
+    for model_id in $(seq 5 50)
+    do
+        # Run the Python command with the current model_id and loss_function
+        python main.py --model_id $model_id --architecture resnet18 --loss $loss_function
+    done
+done
