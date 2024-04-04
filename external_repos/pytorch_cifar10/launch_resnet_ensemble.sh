e1a8471f8d638d286a5b3320408a0e10c9d7bc5b
Nikita Kotelevskii <niket0996@gmail.com>
2024-04-04 10:36:25 +0300

    Minor changes in bash scripts for training ensembles.

--- external_repos/pytorch_cifar10/launch_resnet_ensemble.sh
+++ external_repos/pytorch_cifar10/launch_resnet_ensemble.sh
@@ -6,9 +6,9 @@
 # Outer loop over loss function types
 #for loss_function in "${loss_functions[@]}"
 #do
- loss_function="cross_entropy"
- # Inner loop from 6 to 50 for model_id
-    for model_id in $(seq 6 50)
+loss_function="cross_entropy"
+ # Inner loop from 10 to 20 for model_id
+    for model_id in $(seq 10 20)
     do
         # Run the Python command with the current model_id and loss_function
         CUDA_VISIBLE_DEVICES=0 python main.py --model_id $model_id --architecture resnet18 --loss $loss_function
--- external_repos/pytorch_cifar10/launch_resnet_ensemble_1.sh
+++ external_repos/pytorch_cifar10/launch_resnet_ensemble_1.sh
@@ -6,9 +6,9 @@
 # Outer loop over loss function types
 # for loss_function in "${loss_functions[@]}"
 # do
-    # Inner loop from 0 to 50 for model_id
+    # Inner loop from 0 to 20 for model_id
 loss_function="brier_score"
-    for model_id in $(seq 0 50)
+    for model_id in $(seq 11 20)
     do
         # Run the Python command with the current model_id and loss_function
         CUDA_VISIBLE_DEVICES=1 python main.py --model_id $model_id --architecture resnet18 --loss $loss_function
