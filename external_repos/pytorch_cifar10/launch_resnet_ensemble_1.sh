95a97a971493449990c1198de3c99b1d6fc90df5
Nikita Kotelevskii <niket0996@gmail.com>
2024-04-05 10:45:01 +0300

    Minor changes in launch bash script.

--- external_repos/pytorch_cifar10/launch_resnet_ensemble_1.sh
+++ external_repos/pytorch_cifar10/launch_resnet_ensemble_1.sh
@@ -8,7 +8,7 @@
 # do
     # Inner loop from 0 to 20 for model_id
 loss_function="brier_score"
-    for model_id in $(seq 11 20)
+    for model_id in $(seq 2 20)
     do
         # Run the Python command with the current model_id and loss_function
         CUDA_VISIBLE_DEVICES=1 python main.py --model_id $model_id --architecture resnet18 --loss $loss_function
