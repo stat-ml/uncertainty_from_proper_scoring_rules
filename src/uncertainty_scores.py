187410f7171cb26c24e01a959cb6047388dd292c
Nikita Kotelevskii <nikita.kotelevskii@skoltech.ru>
2024-04-08 22:43:01 +0400

    Added EP-Brier and EP-Spherical

--- src/uncertainty_scores.py
+++ src/uncertainty_scores.py
@@ -33,6 +33,30 @@ def pairwise_kl(logits_p, logits_q):
     return kl_divs
 
 
+def pairwise_brier(logits_p, logits_q):
+    p_exp = safe_softmax(logits_p)[:, np.newaxis, :, :]
+    q_exp = safe_softmax(logits_q)[np.newaxis, :, :, :]
+
+    brier_divs = np.sum((p_exp - q_exp) ** 2, axis=-1)
+
+    return brier_divs
+
+
+def pairwise_spherical(logits_p, logits_q):
+    p_exp = safe_softmax(logits_p)[:, np.newaxis, :, :]
+    q_exp = safe_softmax(logits_q)[np.newaxis, :, :, :]
+
+    p_exp_normed = p_exp / np.linalg.norm(p_exp, ord=2, axis=-1, keepdims=True)
+    q_exp_normed = q_exp / np.linalg.norm(q_exp, ord=2, axis=-1, keepdims=True)
+
+    p_normed = np.linalg.norm(p_exp, ord=2, axis=-1)
+    pairwise_dot = np.sum(p_exp_normed * q_exp_normed, axis=-1)
+
+    spherical_divs = p_normed * (1 - pairwise_dot)
+
+    return spherical_divs
+
+
 def expected_pairwise_kl(
         logits_p: np.ndarray,
         logits_q: np.ndarray
@@ -42,15 +66,29 @@ def expected_pairwise_kl(
     )
 
 
+def expected_pairwise_brier(
+        logits_p: np.ndarray,
+        logits_q: np.ndarray
+) -> np.ndarray:
+    return np.mean(
+        pairwise_brier(logits_p, logits_q), axis=(0, 1)
+    )
+
+
+def expected_pairwise_spherical(
+        logits_p: np.ndarray,
+        logits_q: np.ndarray
+) -> np.ndarray:
+    return np.mean(
+        pairwise_spherical(logits_p, logits_q), axis=(0, 1)
+    )
+
+
 if __name__ == '__main__':
     # Example usage
-    N_members, N_objects, N_classes = 3, 123, 5  # Example dimensions
+    N_members, N_objects, N_classes = 3, 40, 5  # Example dimensions
     A = np.random.randn(N_members, N_objects, N_classes)
     B = np.random.randn(N_members, N_objects, N_classes)
 
-    kl_results = pairwise_kl(A, B)
-    print(kl_results.shape)  # This will show (N_members, N_members, N_objects)
-
-    epkl_results = expected_pairwise_kl(A, B)
-    # This will show (N_members, N_members, N_objects)
-    print(epkl_results.shape)
+    squared_dist_results = expected_pairwise_spherical(A, B)
+    print(squared_dist_results.shape)
