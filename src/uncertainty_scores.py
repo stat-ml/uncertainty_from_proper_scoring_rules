Fixed bugs in risks computation. Still, there is a bug in MaxProb

src/uncertainty_scores.py
@@ -94,12 +94,20 @@ def pairwise_brier(logits_gt, logits_pred):
     Returns:
         _type_: _description_
     """
-    p_exp = safe_softmax(logits_gt)[:, np.newaxis, :, :]
-    q_exp = safe_softmax(logits_pred)[np.newaxis, :, :, :]
+    p_exp = safe_softmax(logits_gt)[np.newaxis, :, :, :]
+    q_exp = safe_softmax(logits_pred)[:, np.newaxis, :, :]
 
     brier_divs = np.sum((p_exp - q_exp) ** 2, axis=-1)
 
     return brier_divs
+
+
+def select_by_indices(array, indices):
+    new_array = np.empty((array.shape[0], array.shape[1]))
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            new_array[i, j] = array[i, j, indices[i, j]]
+    return new_array
 
 
 def pairwise_prob_diff(logits_gt, logits_pred):
@@ -118,11 +126,13 @@ def pairwise_prob_diff(logits_gt, logits_pred):
     argmax_indices = np.argmax(prob_pred, axis=-1)
 
     max_gt = np.max(prob_gt, axis=-1)[:, np.newaxis, :]
+
     gt_pred_index = prob_gt[
         np.arange(argmax_indices.shape[0])[:, None],
         np.arange(argmax_indices.shape[1]),
         argmax_indices
     ][None]
+
     prob_divs = max_gt - gt_pred_index
 
     return prob_divs
@@ -205,10 +215,10 @@ def mutual_information_avg_kl(logits_pred, logits_gt=None):
     Mutual information, computed as Expected KL[pred | ppd]
     """
     prob_p = safe_softmax(logits_pred)
-    avg_predictions = posterior_predictive(logits_=logits_pred)
+    ppd = posterior_predictive(logits_=logits_pred)
     return np.mean(
         np.sum(
-            prob_p * np.log(prob_p / avg_predictions), axis=-1
+            prob_p * np.log(prob_p / ppd), axis=-1
         ), axis=0)
 
 
@@ -284,8 +294,8 @@ def bayes_neglog_outer(logits_gt, logits_pred=None):
 
 
 def bayes_neglog_inner(logits_gt, logits_pred=None):
-    avg_predictions = posterior_predictive(logits_=logits_gt)[0]
-    return np.sum(np.log(avg_predictions), axis=-1)
+    ppd = posterior_predictive(logits_=logits_gt)[0]
+    return np.sum(np.log(ppd), axis=-1)
 
 
 def excess_neglog_outer(
@@ -301,12 +311,11 @@ def excess_neglog_inner(
         logits_gt: np.ndarray,
         logits_pred: np.ndarray
 ) -> np.ndarray:
-    p_exp = posterior_predictive(logits_gt)
+    ppd = posterior_predictive(logits_gt)
     q_exp = safe_softmax(logits_pred)
 
-    is_dist = np.sum(p_exp / q_exp -
-                     logits_gt + logits_pred +
-                     logsumexp(logits_gt, axis=-1, keepdims=True) -
+    is_dist = np.sum(ppd / q_exp -
+                     np.log(ppd) + logits_pred -
                      logsumexp(logits_pred, axis=-1, keepdims=True) - 1,
                      axis=-1
                      )
@@ -341,12 +350,12 @@ def total_spherical(
 
 def bayes_spherical_outer(logits_gt, logits_pred=None):
     prob_p = safe_softmax(logits_gt)
-    return -np.mean(np.linalg.norm(prob_p, axis=-1), axis=0)
+    return -np.mean(np.linalg.norm(prob_p, axis=-1, ord=2), axis=0)
 
 
 def bayes_spherical_inner(logits_gt, logits_pred=None):
-    avg_predictions = posterior_predictive(logits_=logits_gt)[0]
-    return -np.linalg.norm(avg_predictions, axis=-1)
+    ppd = posterior_predictive(logits_=logits_gt)[0]
+    return -np.linalg.norm(ppd, axis=-1, ord=2)
 
 
 def excess_spherical_outer(
@@ -362,7 +371,7 @@ def excess_spherical_inner(
         logits_gt: np.ndarray,
         logits_pred: np.ndarray
 ) -> np.ndarray:
-    ppd = safe_softmax(logits_gt)
+    ppd = posterior_predictive(logits_gt)
     pred = safe_softmax(logits_pred)
 
     ppd_normed = ppd / np.linalg.norm(ppd, ord=2, axis=-1, keepdims=True)
@@ -401,12 +410,12 @@ def total_maxprob(
 
 def bayes_maxprob_outer(logits_gt, logits_pred=None):
     prob_p = safe_softmax(logits_gt)
-    return np.mean(1 - np.max(prob_p, axis=-1), axis=0)
+    return 1 - np.mean(np.max(prob_p, axis=-1), axis=0)
 
 
 def bayes_maxprob_inner(logits_gt, logits_pred=None):
-    avg_predictions = posterior_predictive(logits_=logits_gt)[0]
-    return 1 - np.max(avg_predictions, axis=-1)
+    ppd = posterior_predictive(logits_=logits_gt)[0]
+    return 1 - np.max(ppd, axis=-1)
 
 
 def excess_maxprob_outer(
@@ -423,14 +432,18 @@ def excess_maxprob_inner(
         logits_gt: np.ndarray,
         logits_pred: np.ndarray
 ) -> np.ndarray:
-    avg_predictions = posterior_predictive(logits_=logits_gt)
+    ppd = posterior_predictive(logits_=logits_gt)
     prob_pred = safe_softmax(logits_pred)
 
     argmax_indices = np.argmax(prob_pred, axis=-1)
     objects_index = np.arange(prob_pred.shape[1])
-    max_gt = avg_predictions[:, objects_index, argmax_indices][0]
+    ppd_indexed = ppd[
+        :,
+        objects_index,
+        argmax_indices
+    ][0]
 
-    prob_divs = np.mean(np.max(avg_predictions, axis=-1) - max_gt, axis=0)
+    prob_divs = np.mean(np.max(ppd, axis=-1) - ppd_indexed, axis=0)
 
     return prob_divs
 
@@ -457,14 +470,13 @@ def total_brier(
 
 
 def bayes_brier_outer(logits_gt, logits_pred=None):
-    return 1 - np.mean(
-        np.linalg.norm(logits_gt, ord=2, axis=-1), axis=0
-    )
+    probs = safe_softmax(logits_gt)
+    return 1 - np.mean(np.sum(probs**2, axis=-1), axis=0)
 
 
 def bayes_brier_inner(logits_gt, logits_pred=None):
-    avg_predictions = posterior_predictive(logits_=logits_gt)[0]
-    return 1 - np.linalg.norm(avg_predictions, ord=2, axis=-1)
+    ppd = posterior_predictive(logits_=logits_gt)[0]
+    return 1 - np.sum(ppd**2, axis=-1)
 
 
 def excess_brier_outer(
@@ -472,7 +484,8 @@ def excess_brier_outer(
         logits_pred: np.ndarray
 ) -> np.ndarray:
     return np.mean(
-        pairwise_brier(logits_gt, logits_pred), axis=(0, 1)
+        pairwise_brier(
+            logits_gt=logits_gt, logits_pred=logits_pred), axis=(0, 1)
     )
 
 
@@ -480,11 +493,9 @@ def excess_brier_inner(
         logits_gt: np.ndarray,
         logits_pred: np.ndarray
 ) -> np.ndarray:
-    avg_predictions = posterior_predictive(logits_=logits_pred)
-    pred_prob = safe_softmax(logits_gt)
-    return np.mean(
-        np.linalg.norm(avg_predictions - pred_prob, ord=2, axis=-1), axis=0
-    )
+    ppd = posterior_predictive(logits_=logits_gt)
+    pred_prob = safe_softmax(logits_pred)
+    return np.mean(np.sum((ppd - pred_prob)**2, axis=-1), axis=0)
 
 
 def bi_brier(
@@ -528,10 +539,10 @@ def excess_logscore_inner(logits_gt, logits_pred):
     Reverse mutual information, computed as Expected KL[ppd | pred]
     """
     prob_p = safe_softmax(logits_pred)
-    avg_predictions = posterior_predictive(logits_=logits_gt)
+    ppd = posterior_predictive(logits_=logits_gt)
     return np.mean(
         np.sum(
-            avg_predictions * np.log(avg_predictions / prob_p), axis=-1
+            ppd * np.log(ppd / prob_p), axis=-1
         ), axis=0)
 
 
@@ -541,8 +552,8 @@ def bayes_logscore_outer(logits_gt, logits_pred=None):
 
 
 def bayes_logscore_inner(logits_gt, logits_pred=None):
-    avg_predictions = posterior_predictive(logits_=logits_gt)[0]
-    return entropy(avg_predictions)
+    ppd = posterior_predictive(logits_=logits_gt)[0]
+    return entropy(ppd)
 
 
 def bi_logscore(
@@ -557,12 +568,46 @@ def bi_logscore(
 
 if __name__ == '__main__':
     # Example usage
-    N_members, N_objects, N_classes = 3, 40, 5  # Example dimensions
+    N_members, N_objects, N_classes = 5, 100, 1000  # Example dimensions
     A = np.random.randn(N_members, N_objects, N_classes)
     B = np.random.randn(N_members, N_objects, N_classes)
 
-    # squared_dist_results = bayes_neglog_inner(A, B)
-    # print(squared_dist_results.shape)
+    print('Testing that Bayes outer is less than Bayes inner')
+    for score, b_i, b_o in [
+        ("Logscore", bayes_logscore_inner, bayes_logscore_outer),
+        ("Brier", bayes_brier_inner, bayes_brier_outer),
+        ("Spherical", bayes_spherical_inner, bayes_spherical_outer),
+        ("Neglog", bayes_neglog_inner, bayes_neglog_outer),
+        ("MaxProb", bayes_maxprob_inner, bayes_maxprob_outer),
+    ]:
+        try:
+            assert np.all(b_o(A, B) <= b_i(A, B))
+            print(f"{score} OK")
+        except Exception as ex:
+            print(f"{score} FAILED {ex}")
+    print("Success!")
+    print("*" * 30)
+
+    print('Testing that inner and outer approximations lead to the same Total')
+    for score, b_i, b_o, e_i, e_o in [
+        ("Logscore", bayes_logscore_inner, bayes_logscore_outer,
+         excess_logscore_inner, excess_logscore_outer),
+        ("Brier", bayes_brier_inner, bayes_brier_outer,
+         excess_brier_inner, excess_brier_outer),
+        ("Spherical", bayes_spherical_inner, bayes_spherical_outer,
+         excess_spherical_inner, excess_spherical_outer),
+        ("Neglog", bayes_neglog_inner, bayes_neglog_outer,
+         excess_neglog_inner, excess_neglog_outer),
+        ("Maxprob", bayes_maxprob_inner, bayes_maxprob_outer,
+         excess_maxprob_inner, excess_maxprob_outer),
+    ]:
+        try:
+            tol_i = b_i(A, B) + e_i(A, B)
+            tol_o = b_o(A, B) + e_o(A, B)
+            assert np.all(np.isclose(tol_i, tol_o))
+            print(f"{score} OK")
+        except Exception as ex:
+            print(f"{score} FAILED {ex}")
 
     for func_ in [
         total_brier,