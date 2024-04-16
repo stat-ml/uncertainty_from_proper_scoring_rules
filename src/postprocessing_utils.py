0404f9ebaba357bed059653fd984ba7832f450d4
Nikita Kotelevskii <nikita.kotelevskii@skoltech.ru>
2024-04-16 15:11:52 +0400

    The rest of the functions from notebook transferred to postprocessing_utils.py

--- src/postprocessing_utils.py
+++ src/postprocessing_utils.py
@@ -1,10 +1,11 @@
+import re
 import pandas as pd
 import numpy as np
-import os
 from tqdm.auto import tqdm
-from collections import defaultdict
+from typing import Optional
+from sklearn.metrics import roc_auc_score
 from src.evaluation_utils import collect_stats
-from evaluation_utils import make_load_path, load_dict, collect_embeddings
+from evaluation_utils import collect_embeddings
 from uncertainty_scores import (
     total_brier,
     total_logscore,
@@ -169,7 +170,8 @@ def get_uncertainty_scores(
                     dataset_=dataset_,
                 )
 
-                uq_results[uq_name][loss_function_name][dataset_] = uncertainty_func(
+                uq_results[uq_name][loss_function_name][dataset_] = \
+                    uncertainty_func(
                     logits_gt=ground_truth_embeddings,
                     logits_pred=embeddings_per_dataset[dataset_]
                 )
@@ -229,3 +231,244 @@ def get_predicted_labels(
         axis=-1
     )
     return pred_labels
+
+
+def get_ood_detection_dataframe(
+    ind_dataset: str,
+    uq_results: dict,
+    list_ood_datasets: list[str],
+) -> pd.DataFrame:
+    """
+    The function transforms uq_results dict into pd.Dataframe
+    with ROC AUC scores of OOD detection.
+    """
+    roc_auc_dict = {}
+
+    for uq_name, _ in uq_funcs_with_names:
+        roc_auc_dict[uq_name] = {}
+        print(f'OOD computed via {uq_name}')
+
+        for ood_dataset in list_ood_datasets:
+            roc_auc_dict[uq_name][ood_dataset] = {}
+            for loss_ in uq_results[uq_name].keys():
+                y_true = np.hstack(
+                    [
+                        np.ones(uq_results[uq_name][loss_]
+                                [ood_dataset].shape[0]),
+                        np.zeros(uq_results[uq_name][loss_]
+                                 [ind_dataset].shape[0]),
+                    ]
+                )
+                y_score = np.hstack(
+                    [
+                        uq_results[uq_name][loss_][ood_dataset],
+                        uq_results[uq_name][loss_][ind_dataset],
+                    ]
+                )
+                score = roc_auc_score(y_true=y_true, y_score=y_score)
+                roc_auc_dict[uq_name][ood_dataset][loss_] = score
+
+                print(
+                    (
+                        f'InD: {ind_dataset} \t '
+                        f'OOD: {ood_dataset} \t '
+                        f'loss: {loss_} \t '
+                        f'roc_auc: {score}'
+                    )
+                )
+
+    data_list = []
+    for metric_name, datasets in roc_auc_dict.items():
+        for dataset_name, loss_functions in datasets.items():
+            for loss_function_name, value in loss_functions.items():
+                data_list.append((metric_name, dataset_name,
+                                 loss_function_name, value))
+
+    # Create a DataFrame
+    df = pd.DataFrame(data_list, columns=[
+                      "UQMetric", "Dataset", "LossFunction", "RocAucScore"])
+
+    return df
+
+
+def get_missclassification_dataframe(
+    ind_dataset: str,
+    uq_results: dict,
+    true_labels: np.ndarray,
+    pred_labels: np.ndarray,
+) -> pd.DataFrame:
+    """
+    The function transforms uq_results dict into pd.Dataframe
+    with ROC AUC scores of misclassification detection.
+    """
+    roc_auc_dict = {}
+
+    for uq_name, _ in uq_funcs_with_names:
+        roc_auc_dict[uq_name] = {}
+        print(f'Misclassification computed via {uq_name}')
+
+        for loss_ in uq_results[uq_name].keys():
+            y_true = (true_labels != pred_labels).astype(np.int32)
+            y_score = uq_results[uq_name][loss_][ind_dataset]
+
+            score = roc_auc_score(y_true=y_true, y_score=y_score)
+            roc_auc_dict[uq_name][loss_] = score
+
+            print(
+                f'InD: {ind_dataset} \t loss: {loss_} \t roc_auc: {score}')
+
+    data_list_misclassification = []
+    for metric_name, loss_function in roc_auc_dict.items():
+        for loss_function_name, value in loss_function.items():
+            data_list_misclassification.append(
+                (metric_name, loss_function_name, value))
+
+    # Create a DataFrame
+    df_misclassification = pd.DataFrame(
+        data_list_misclassification,
+        columns=[
+            "UQMetric",
+            "LossFunction",
+            "RocAucScore",
+        ]
+    )
+    return df_misclassification
+
+
+def make_aggregation(
+    df_: pd.DataFrame,
+    prefix_: str,
+    suffix_: Optional[str],
+    dataset_: Optional[str] = None,
+    by_loss_function_: bool = False
+) -> pd.DataFrame:
+    """
+    The function aggregates given pd.DataFrame in different ways
+    prefix_ used to select a proper UQ name (Total, Excess, Bayes, Bregman..)
+    suffix_ used to specify Inner or Outer. If None, both are aggregated
+    dataset_ used to specify a concrete dataset
+    by_loss_function_ if True, aggregation by Loss function is used
+    """
+    df = pd.DataFrame.copy(df_)
+    if dataset_ is None:
+        dataset_mask = np.ones(len(df))
+    else:
+        dataset_mask = df['Dataset'].str.fullmatch(dataset_)
+    if suffix_ is not None:
+        filtered_df = df[dataset_mask & df['UQMetric'].str.startswith(
+            prefix_) & df['UQMetric'].str.endswith(suffix_)]
+        if by_loss_function_:
+            roc_auc_scores = filtered_df.groupby(
+                ['UQMetric', 'LossFunction']
+            ).agg({'RocAucScore': ['mean', 'std', 'count']})
+        else:
+            roc_auc_scores = filtered_df.groupby(['UQMetric',]).agg({
+                'RocAucScore': ['mean', 'std', 'count']
+            })
+    else:
+        filtered_df = df[dataset_mask & df['UQMetric'].str.startswith(prefix_)]
+        filtered_df.loc[:, 'GeneralizedMetric'] = \
+            filtered_df['UQMetric'].apply(
+                lambda x: re.sub(r'\s+(Inner|Outer)$', '', x)
+        )
+
+        # Group by 'GeneralizedMetric' and calculate mean ROC AUC score
+        if by_loss_function_:
+            roc_auc_scores = \
+                filtered_df.groupby(
+                    ['GeneralizedMetric', 'LossFunction']
+                ).agg({'RocAucScore': ['mean', 'std', 'count']})
+        else:
+            roc_auc_scores = filtered_df.groupby(['GeneralizedMetric',]).agg({
+                'RocAucScore': ['mean', 'std', 'count']
+            })
+    # roc_auc_scores = \
+    # filtered_df.groupby(['UQMetric', 'LossFunction'])['RocAucScore'].mean()
+
+    roc_auc_df = roc_auc_scores.reset_index()
+    roc_auc_df.rename(columns={'RocAucScore': 'MeanRocAucScore'}, inplace=True)
+    return roc_auc_df
+
+
+def get_raw_scores_dataframe(
+    uq_results: dict,
+):
+    # Convert the nested dictionary into a DataFrame
+    data = []
+    for uq_name, loss_funcs in uq_results.items():
+        for loss_name, datasets in loss_funcs.items():
+            for dataset_name, scores in datasets.items():
+                data.append({
+                    'UQ Method': uq_name,
+                    'Loss Function': loss_name,
+                    'Dataset': dataset_name,
+                    'Scores': scores
+                })
+
+    df = pd.DataFrame(data)
+    return df
+
+
+def ravel_df(
+        df: pd.DataFrame,
+        loss_function: str,
+        dataset: str,
+        score: str,
+) -> pd.DataFrame:
+    """
+    The function takes dataframe where score value is np.ndarray
+    and transforms it in a way that it will be a column.
+    It also selects a specific score, dataset and loss_function
+    """
+    tot = df[
+        (
+            df['Loss Function'].str.startswith(loss_function) &
+            df['Dataset'].str.startswith(dataset) &
+            df['UQ Method'].str.startswith(f'Total {score}')
+        )].Scores.values[0]
+
+    exc_o = df[
+        (df['Loss Function'].str.startswith(loss_function) &
+         df['Dataset'].str.startswith(dataset) &
+         df['UQ Method'].str.startswith(f'Excess {score} Outer')
+         )].Scores.values[0]
+    exc_i = df[
+        (df['Loss Function'].str.startswith(loss_function) &
+         df['Dataset'].str.startswith(dataset) &
+         df['UQ Method'].str.startswith(f'Excess {score} Inner')
+         )].Scores.values[0]
+
+    bay_o = df[
+        (df['Loss Function'].str.startswith(loss_function) &
+         df['Dataset'].str.startswith(dataset) &
+         df['UQ Method'].str.startswith(f'Bayes {score} Outer')
+         )].Scores.values[0]
+    bay_i = df[
+        (df['Loss Function'].str.startswith(loss_function) &
+         df['Dataset'].str.startswith(dataset) &
+         df['UQ Method'].str.startswith(f'Bayes {score} Inner')
+         )].Scores.values[0]
+
+    bregman = df[
+        (df['Loss Function'].str.startswith(loss_function) &
+         df['Dataset'].str.startswith(dataset) &
+         df['UQ Method'].str.startswith(f'Bregman Information {score}')
+         )].Scores.values[0]
+
+    scores_dict_ = {
+        'Total': tot,
+
+        "Excess outer": exc_o,
+        "Bayes outer": bay_o,
+
+        "Excess inner": exc_i,
+        "Bayes inner": bay_i,
+
+        "BregmanInformation": bregman,
+
+        'Total outer': exc_o + bay_o,
+        'Total inner': exc_i + bay_i,
+    }
+    scores_df = pd.DataFrame.from_dict(scores_dict_)
+
+    return scores_df
