de99f2d6266aba2fd738590e1843a593ceeb267e
Nikita Kotelevskii <niket0996@gmail.com>
2024-04-20 19:06:21 +0300

    Small change in config

--- external_repos/pytorch_cifar10/launch_resnet_ensemble_1.sh
+++ external_repos/pytorch_cifar10/launch_resnet_ensemble_1.sh
@@ -9,7 +9,7 @@
 #do
 loss_function="brier_score"
  # Inner loop from 10 to 20 for model_id
-    for model_id in $(seq 0 20)
+    for model_id in $(seq 19 20)
     do
         # Run the Python command with the current model_id and loss_function
         CUDA_VISIBLE_DEVICES=1 python main.py --model_id $model_id --architecture resnet18 --loss $loss_function --dataset noisy_cifar10
